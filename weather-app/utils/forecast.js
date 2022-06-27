8ff8b14cf9b10f685157e1864e7ef02f5260619f
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-06-27 11:45:07 -0400

    restructuring the app and making modules taking cli input

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -20,13 +20,13 @@ const request = require("postman-request");
 
 const forecast = (long, lat, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=d8fd5256e0284f763013d73b0ad7498f&query=${lat},${long}&units=f`;
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to newtwork", undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback("Unable to find location", undefined);
     } else {
-      const details = response.body.current;
+      const details = body.current;
       callback(undefined, {
         currentTemp: details.temperature,
         feelsLike: details.feelslike,
