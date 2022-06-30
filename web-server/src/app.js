40e8292034024ce80f1e5b3614925b9a25c59b1e
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-06-30 18:36:16 -0400

    utilizing express and hbs for custom server and custom views

---
+++ web-server/src/app.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const express = require("express");
+const { application } = require("express");
+
+// console.log(__dirname);
+// console.log(path.join(__dirname, "../public"));
+const app = express();
+const publicDirectory = path.join(__dirname, "../public");
+// update the views path
+const viewsPath = path.join(__dirname, "../templates");
+
+// to use handlebar via hbs we have to update settings on express
+// takes in key value pair
+app.set("view engine", "hbs");
+
+// setting the customized view path
+app.set("views", viewsPath);
+
+// setup directory for express
+app.use(express.static(publicDirectory));
+
+// root url
+// serve hbs file
+app.get("", (req, res) => {
+  // allows to render views(name of view to render, object of values to be accessed by that view)
+  res.render("index", {
+    title: "Weather App",
+    name: "Saahiil",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    name: "Saahiil",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    message: "This is the help message",
+  });
+});
+// app.get("/help", (req, res) => {
+//     res.send("This is help");
+// });
+// app.get("/about", (req, res) => {
+//     res.send("<h1>ABOUT</h1>");
+// });
+
+app.get("/weather", (req, res) => {
+  // send to browser
+  res.send({ forecast: "it is 40", location: "New York" });
+});
+
+// start server and port number
+app.listen(3000, () => {
+  console.log("server is listening on port 3000");
+});
