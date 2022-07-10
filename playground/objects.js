2586fd4f834aa6fb326c4d364fb2859630779f99
SaahiilM <47355754+SaahiilM@users.noreply.github.com>
2022-07-10 05:00:48 -0400

    creating .gitignore

--- playground/objects.js
+++ playground/objects.js
@@ -23,8 +23,9 @@ const product = {
 // console.log(productLabel, price, stock, rating);
 
 // destructuring inline
-const transaction = (type, { label, stock }) => {
+const transaction = (type, { label, stock = 0 } = {}) => {
   console.log(type, label, stock);
 };
 
 transaction("order", product);
+transaction("order");
